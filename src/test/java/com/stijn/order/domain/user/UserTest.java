16b425ebaf6183cbe6605421464c04d83fa27f87
St12n <stijn.schouppe@hotmail.com>
2022-10-28 11:23:44 +0200

    Added some tests for edge cases

--- src/test/java/com/stijn/order/domain/user/UserTest.java
+++ src/test/java/com/stijn/order/domain/user/UserTest.java
@@ -22,14 +22,47 @@ class UserTest {
         assertThat(user.getLastname().equals("Vercruysse"));
         assertThat(user.getEmail().equals("timv@test.be"));
         assertThat(user.getAddress()).isNotNull();
-        assertThat(user.getPhoneNumber()).isNotNull();
+        assertThat(user.getPhoneNumber().getLocalPhoneNumber()).isEqualTo("0478945");
+        assertThat(user.getPhoneNumber().getCountrycode()).isEqualTo("0032");
+        assertThat(user.getAddress().getStreetname()).isEqualTo("Timstraat");
+        assertThat(user.getAddress().getHousenumber()).isEqualTo("123a");
+        assertThat(user.getAddress().getBoxNumber()).isEqualTo("1");
+        assertThat(user.getAddress().getCity()).isEqualTo("Leuven");
+        assertThat(user.getAddress().getPostalCode()).isEqualTo("4568");
+        assertThat(user.getRole().containsFeature(Feature.LOGIN)).isTrue();
     }
 
     @Test
-    void whenCreatingUserWithNullCodePhone_GivesException() {
+    void whenCreatingCustomerWithSameAddress_AddressIsEqual() {
+        User user = new User(
+                "Tim",
+                "Vercruysse",
+                "timv@test.be",
+                new PhoneNumber("0032", "0478945"),
+                new Address("Timstraat", "123a", "1", "Leuven", "4568"), Role.USER);
+
+        User user2 = new User(
+                "Tim",
+                "Vercruysse",
+                "timv@test.be",
+                new PhoneNumber("0032", "0478945"),
+                new Address("Timstraat", "123a", "1", "Leuven", "4568"), Role.USER);
+
+        assertThat(user.getAddress()).isEqualTo(user2.getAddress());
+    }
+
+    @Test
+    void whenCreatingUserWithNullCountryCodePhone_GivesException() {
         assertThatThrownBy(() -> new User("Test", "Lastname", "test@Test.be", new PhoneNumber(null, "123456789"), new Address("Aalst"), Role.USER))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("Please provide a country code");
+    }
+
+    @Test
+    void whenCreatingUserWithNullLocalPhone_GivesException() {
+        assertThatThrownBy(() -> new User("Test", "Lastname", "test@Test.be", new PhoneNumber("+32", null), new Address("Aalst"), Role.USER))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Please provide a phonenumber");
     }
     @Test
     void whenCreatingUserWithWrongCountryCodePhone_GivesException() {
@@ -51,4 +84,5 @@ class UserTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("please provide a correct country code");
     }
+
 }
